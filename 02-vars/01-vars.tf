f192d6778d3f2d1ad1e9b472686bc0274961d251
azharinfotech001 <azharinfotech001@gmail.com>
2020-06-07 20:34:48 -0400

    varibale first program

---
+++ 02-vars/01-vars.tf
@@ -0,0 +1,12 @@
+# terraform supports three data types
+  1. string
+  2. number
+  3. boolean
+
+variable "image_id" {
+        //Data Type
+        default = 100
+}
+
+output "NUMBER"
+  value = var.NUMBER
